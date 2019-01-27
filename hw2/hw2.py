8cc0e405bb30f025acd7a82fe09f8c6ee49c53bb
spencer <spencer.hann@gmail.com>
2019-01-27 00:16:26 -0800

    working on adding bias to hidden layer

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -7,8 +7,8 @@ py_name = sys.argv[0]
 module_name = sys.argv[0][:-3] # removes ".py"
 cy_name = module_name + ".pyx"
 if len(sys.argv) == 1: # no command line args
-    print(sys.argv[0] + " running setup with default args")
-    print("\t adding \"build_ext\" and \"--inplace\" to sys.argv")
+    print(cy_name + " compiling with default args")
+    print("\tadding \"build_ext\" and \"--inplace\" to sys.argv")
     sys.argv.extend( ["build_ext", "--inplace"] )
 
 
@@ -38,16 +38,38 @@ from hw2 import * #change module name
 if __name__ == "__main__":
     training_examples, training_targets = preprocess_data(
             "mnist_train.csv", max_rows=1000)
-    testing_examples, testning_targets = preprocess_data(
-            "mnist_test.csv")#, max_rows=500)
+    testing_examples, testing_targets = preprocess_data(
+            "mnist_test.csv", max_rows=1000)
 
-    # +1 for bias weight
-    n_hidden = 1 + 10 # 10, 20, 100
-    n_input = 1 + 784 # fixed
-    n_output = 10 # fixed
-    weights_x = np.random.rand(n_hidden, n_input)
-    weights_h = np.random.rand(n_output, n_hidden)
-    weights_x -= .5 # range was [0,1]
-    weights_h -= .5 # is now [-.5,.5]
+    network10 = NN(n_hidden=10)
+    network20 = NN(n_hidden=20)
+    network100 = NN(n_hidden=100)
 
+    print("\nhidden units = 10")
+    network10.speed_test(training_examples,
+                        training_targets,
+                        testing_examples,
+                        testing_targets)
 
+    print("\nhidden units = 20")
+    network20.speed_test(training_examples,
+                        training_targets,
+                        testing_examples,
+                        testing_targets)
+
+#    print("\nhidden units = 100")
+#    network100.speed_test(training_examples,
+#                        training_targets,
+#                        testing_examples,
+#                        testing_targets)
+
+#    # +1 for bias weight
+#    n_hidden = 10 # 10, 20, 100
+#    n_input = 1 + 784 # fixed
+#    n_output = 10 # fixed
+#    weights_x = np.random.rand(n_hidden, n_input)
+#    weights_h = np.random.rand(n_output, n_hidden)
+#    weights_x -= .5 # range was [0,1]
+#    weights_h -= .5 # is now [-.5,.5]
+#
+#    print(get_accuracy(weights_x,weights_h,training_examples,training_targets))
--- hw2/hw2.pyx
+++ hw2/hw2.pyx
@@ -1,5 +1,6 @@
 # cython: boundscheck=False, wraparound=False, nonecheck=False
 
+import sys
 import numpy as np
 cimport numpy as np
 from cython.parallel cimport prange
@@ -10,7 +11,6 @@ def preprocess_data(filename, max_rows=None):
     cdef np.ndarray data = np.genfromtxt(filename, delimiter=',', max_rows=max_rows)
 
     np.random.shuffle(data)
-    # use dtype=np.intc for compatibility w/ C layer
     cdef np.ndarray targets = data[:,0].astype(np.intc)
     data[1:,:] /= 255 # all values into [0,1] range
     data[:,0] = 1 # adds bias(=1) column
@@ -29,18 +29,21 @@ cdef void activate_vector(double[:] vector, Py_ssize_t vector_size) nogil:
     for i in prange(vector_size,nogil=True):
         vector[i] = sigmoid(vector[i])
 
+cdef inline double get_output_error(double output, int target) nogil:
+    return output * (1-output) * (target - output)
+
 cdef class NN: # Neural_Network:
     cdef double[:,::1] weights_x
     cdef double[:,::1] weights_h
-    cdef double[::1] hidden_layer
-    cdef double[::1] output_layer
+    cdef np.ndarray hidden_layer
+    cdef np.ndarray output_layer
     cdef int n_hidden
     cdef int n_input
     cdef int n_output
     cdef double momentum
     cdef double learning_rate
 
-    def __init__(
+    def __cinit__(
             NN self,
             int n_hidden = 10,
             int n_input = 1 + 784,
@@ -54,25 +57,23 @@ cdef class NN: # Neural_Network:
         self.momentum = momentum
         self.learning_rate = learning_rate
 
-        weights_x = np.random.rand(n_hidden, n_input)
-        weights_h = np.random.rand(n_output, n_hidden)
-        weights_x -= .5 # range was [0,1]
-        weights_h -= .5 # is now [-.5,.5]
+        self.weights_x = np.random.rand(n_hidden, n_input) - .5
+        self.weights_h = np.random.rand(n_output, n_hidden+1) - .5
 
-        self.hidden_layer = np.empty(n_hidden, dtype=np.float)
+        self.hidden_layer = np.empty(n_hidden+1, dtype=np.float)
+        self.hidden_layer[0] = 1.0
         self.output_layer = np.empty(n_output, dtype=np.float)
 
     cdef void feed_forward(NN self, double[::1] inputs):
-        np.matmul(inputs, self.weights_x, out=self.hidden_layer)
-        activate_vector(self.hidden_layer, self.n_hidden)
+        np.matmul(self.weights_x, inputs, out=self.hidden_layer[1:])
+        activate_vector(self.hidden_layer[1:], self.n_hidden)
 
-        np.matmul(self.hidden_layer, self.weights_h, out=self.output_layer)
+        np.matmul(self.weights_h, self.hidden_layer, out=self.output_layer)
         activate_vector(self.output_layer, self.n_output)
 
     cdef int get_result(NN self, double[::1] inputs):
-        cdef double[::1] results
-        results = self.feed_forward(inputs)
-        return np.argmax(results)
+        self.feed_forward(inputs)
+        return np.argmax(self.output_layer)
 
     cdef double get_accuracy(
             NN self,
@@ -83,38 +84,91 @@ cdef class NN: # Neural_Network:
         cdef int[::1] results = np.zeros(targets.shape[0], dtype=np.intc)
         cdef Py_ssize_t i
 
-        print "testing", len(examples)
         for i in range(len(examples)):
-            result = get_result(self.weights_x,self.weights_h,examples[i])
+            result = self.get_result(examples[i])
             results[i] = targets[i] == result
-
-        print("meaning")
 
         return np.mean(results)
 
-    cdef void back_prop(NN self, int[::1] targets):
-        cdef Py_ssize_t k, j
-        cdef double[::1] hidden_errors = np.empty(n_hidden)
-        cdef double[::1] output_errors = np.empty(n_output)
-        cdef double[::1] target = np.zeros(n_output)
-        cdef double[::1] prev_weight_x_changes = np.zeros(n_input)
-        cdef double[::1] prev_weight_h_changes = np.zeros(n_hidden)
+    cdef void back_prop(NN self, double[:,::1] inputs, int[::1] targets):
+        cdef Py_ssize_t i, k, j
+        cdef int[::1] target = np.zeros(self.n_output, dtype=np.intc)
+        cdef double[::1] errors_xh = np.empty(self.n_hidden)
+        cdef double[::1] errors_ho = np.empty(self.n_output)
+        cdef double[::1] prev_weight_x_change = np.zeros(self.n_input)
+        cdef double[::1] prev_weight_h_change = np.zeros(self.n_hidden)
+        cdef double weighted_output_error
+        cdef double weight_change
+        # cdef typing numpy arrays as memoryviews allows access w/o gil
+        cdef double[::1] hidden_layer = self.hidden_layer
+        cdef double[::1] output_layer = self.output_layer
 
-        for i in range(targes.shape[0]):
-            target[targets[i]] = 1
+        for n in range(targets.shape[0]):
+            target[targets[n]] = 1
+            self.feed_forward(inputs[n])
+            hidden_layer = self.hidden_layer
+
+           ## Output Layer loop
             # determine error from hidden layer to output layer
             for k in prange(self.n_output, nogil=True):
-                output_errors[k] = self.output_layer[k] * \
-                                (1-self.output_layer[k]) * \
-                                (target[k] - self.output_layer[k])
+                errors_ho[k] = get_output_error(output_layer[k], target[k])
 
+           ## Hidden->Output Layer loop
             # determine error from input layer to hidden layer
+            # while in j loop, update weights
+            for j in prange(self.n_hidden+1, nogil=True):
+                weighted_output_error = 0.0
+                for k in prange(self.n_output):
+                    weighted_output_error += self.weights_h[k,j] * errors_ho[k]
+
+                    # update weights_h for every jth hidden 
+                    # unit and every kth output unit
+                    weight_change = self.learning_rate * \
+                                    errors_ho[k] * \
+                                    hidden_layer[j] + \
+                                    self.momentum * \
+                                    prev_weight_h_change[j]
+                    prev_weight_h_change[j] = weight_change
+                    self.weights_h[k,j] += weight_change
+
+           ## Input->Hidden Layer loop
+            hidden_layer = hidden_layer[1:] # rm bias
             for j in prange(self.n_hidden, nogil=True):
-                for k in prange(n_output):
-                    weighted_output_error += self.weights_h[k,j] * output_errors[k]
-                hidden_errors[j] = self.hidden_layer[j] * \
-                                (1-self.hidden_layer[j]) * \
-                                weighted_ouput_error
+                # error from input layer to hidden layer
+                errors_xh[j] = hidden_layer[j] * \
+                                (1-hidden_layer[j]) * \
+                                weighted_output_error
 
+                # update weights_x for every ith input 
+                # feature and jth hidden unit
+                for i in prange(self.n_input):
+                    weight_change = self.learning_rate * \
+                                    errors_xh[j] * \
+                                    inputs[n,i] + \
+                                    self.momentum * \
+                                    prev_weight_x_change[i]
+                    prev_weight_x_change[i] = weight_change
+                    self.weights_x[j,i] += weight_change
 
-            target[targets[i]] = 0
+            target[targets[n]] = 0
+
+    def speed_test(self,
+            double[:,::1] training_examples,
+            int[::1] training_targets,
+            double[:,::1] testing_examples,
+            int[::1] testing_targets
+            ):
+        print "initial accuracy: "
+        print self.get_accuracy(testing_examples, testing_targets)
+        print "..."
+
+        cdef int epoch
+        for epoch in range(50):
+            self.back_prop(training_examples, training_targets)
+            epoch += 1
+            print epoch,
+            sys.stdout.flush()
+            if epoch % 10 == 0:
+                print "\nEpoch: " + str(epoch)
+                print self.get_accuracy(training_examples, training_targets)
+                print self.get_accuracy(testing_examples, testing_targets)
